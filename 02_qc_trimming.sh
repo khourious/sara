cc4373254355c3007bb8a189d408061a626e5295
Joyce Karoline <83780948+joyce-karol@users.noreply.github.com>
2026-03-16 09:18:30 -0300

    Update input directory for fastp processing

--- 02_qc_trimming.sh
+++ 02_qc_trimming.sh
@@ -37,18 +37,18 @@ done
 
 # --- MultiQC para consolidar relatórios
 echo "Rodando MultiQC..."
-multiqc "$fastqc_dir" -o "$qc_dir"
+multiqc "$fastqc_dir" -o "$fastqc_dir"
 
 
 # --- Filtar e Trimming
-for file in "$input_dir"/*.R*.fastq.gz; do
+for file in "$concat_dir"/*.R*.fastq.gz; do
     sample_name=$(basename "$file" | sed 's/.R[12].fastq.gz//')
 
     echo "Processando $sample_name..."
 
     fastp -f 1 -t 1 -3 -5 -W 4 -M 25 -l 40 \ #adapt for your dataset
-        --in1 "$input_dir"/"$sample_name".R1.fastq.gz \
-        --in2 "$input_dir"/"$sample_name".R2.fastq.gz \
+        --in1 "$concat_dir"/"$sample_name".R1.fastq.gz \
+        --in2 "$concat_dir"/"$sample_name".R2.fastq.gz \
         --out1 "$fastp_dir"/"$sample_name"_FP.R1.paired.fastq.gz \
         --out2 "$fastp_dir"/"$sample_name"_FP.R2.paired.fastq.gz \
         --unpaired1 "$fastp_dir"/"$sample_name"_FP.R1.unpaired.fastq.gz \
